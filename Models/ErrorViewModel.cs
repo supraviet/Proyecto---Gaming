74723a12b9c1576e8330849bf97639ff489ed0db
Sebastian <josebastian245@gmail.com>
2025-08-28 02:43:26 -0500

    Base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Proyecto___Gaming.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
